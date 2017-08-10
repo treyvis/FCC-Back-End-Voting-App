9bfd39be9ce7e8da10639c4cc44acd0779f35776
Trey <treyvis@gmail.com>
2017-08-10 11:30:20 -0600

    Adding react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Menu } from 'semantic-ui-react';
+import { BrowserRouter} from 'react-router-dom';
 import Polls from './pages/Polls.js';
 import Poll from './pages/Poll.js';
 import Login from './pages/Login.js';
@@ -9,18 +10,20 @@ import SignUp from './pages/SignUp.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Menu inverted>
-          <Menu.Item name='Polls' active='true'/>
-          <Menu.Item name='New Poll' />
-          <Menu.Item name='Login' />
-          <Menu.Item name='Sign Up' />
-        </Menu>
-        <Polls />
-        <Poll />
-        <Login />
-        <SignUp />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Menu inverted>
+            <Menu.Item name='Polls' active='true'/>
+            <Menu.Item name='New Poll' />
+            <Menu.Item name='Login' />
+            <Menu.Item name='Sign Up' />
+          </Menu>
+          <Polls />
+          <Poll />
+          <Login />
+          <SignUp />
+        </div>
+      </BrowserRouter>
     );
   }
 }
