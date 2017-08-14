1a942aeba203aec6c4625f362ff52edeb4ac2e44
Trey <treyvis@gmail.com>
2017-08-14 17:06:42 -0600

    Routing for newPoll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Menu } from 'semantic-ui-react';
-import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link, Redirect} from 'react-router-dom';
 import Polls from './pages/Polls.js';
 import Poll from './pages/Poll.js';
 import Login from './pages/Login.js';
@@ -15,16 +15,19 @@ class App extends Component {
         <div className="App">
           <Menu inverted>
             <Link to='/polls'><Menu.Item active name='Polls' /></Link>
-            <Menu.Item name='New Poll' />
+            <Link to='/newpoll'><Menu.Item name='New Poll' /></Link>
             <Link to='/login'><Menu.Item name='Login' /></Link>
             <Link to='/signup'><Menu.Item name='Sign Up' /></Link>
           </Menu>
           <Switch>
-            <Route exact path='/' component={NewPoll} />
+            <Route exact path='/'>
+              <Redirect to='/polls' />
+            </Route>
             <Route exact path='/polls' component={Polls} />
             <Route path='/polls/:id' component={Poll} />
             <Route path='/login' component={Login} />
             <Route path='/signup' component={SignUp} />
+            <Route path='/newpoll' component={NewPoll} />
           </Switch>
         </div>
       </BrowserRouter>
