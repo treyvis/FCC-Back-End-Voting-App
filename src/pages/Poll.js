c0983514c1082d30b71f6f597d65caf9c83a2f6d
Trey <treyvis@gmail.com>
2017-08-15 08:53:09 -0600

    Poll loading dynamic

--- src/pages/Poll.js
+++ src/pages/Poll.js
@@ -1,40 +1,35 @@
 import React, { Component } from 'react';
 import { Card, Button, Label } from 'semantic-ui-react';
+import polls from '../database/polls.json';
 
 class Poll extends Component {
 	render(){
 
-		console.log(this.props.match);
+		const pollId = this.props.match.params.id;
+
+		const choicesList = polls.data[pollId].choices.map((choice, index) => {
+			return (
+			  <Button primary floated='left' key={index}>
+                <Label horizontal>
+                  {choice.count}
+                </Label>
+                {choice.name}
+              </Button>
+			);
+		});
 
 		return(
 			<Card>
 	          <Card.Content>
 	            <Card.Header>
-	              Best Lord of the Rings Movie
+	              {polls.data[pollId].title}
 	            </Card.Header>
 	            <Card.Description>
-	              Choose which film rules them all.
+	              {polls.data[pollId].description}
 	            </Card.Description>
 	            <Card.Content extra >
-	            	<Button.Group vertical>
-		              <Button primary floated='left'>
-		                <Label horizontal>
-		                  203
-		                </Label>
-		                The Fellowship of the Ring
-		              </Button>
-		              <Button primary floated='left'>
-		                <Label horizontal>
-		                  187
-		                </Label>
-		                The Two Towers
-		              </Button>
-		              <Button primary>
-		                <Label horizontal>
-		                  471
-		                </Label>
-		                The Return of the King
-		              </Button>
+	            	<Button.Group vertical fluid>
+		              {choicesList}
 		            </Button.Group>
 	            </Card.Content>
 	          </Card.Content>
