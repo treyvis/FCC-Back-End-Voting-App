d25ce736356db81ae286dcb3b12df2838e50166b
Trey <treyvis@gmail.com>
2017-08-16 17:02:56 -0600

    Setting up api route in express for db access

--- src/server/server.js
+++ src/server/server.js
@@ -5,14 +5,6 @@ const mongo = require('mongodb').MongoClient;
 const config = require('./config.json');
 const mongoURI = 'mongodb://' + config.user + ':' + config.password + '@ds123351.mlab.com:23351/fcc-voting-app';
 
-mongo.connect(mongoURI, (err, db) => {
-	if (err) throw err;
-	console.log('Connected to database');
-	db.collection('polls').find().toArray((err, docs) => {
-		console.log(docs);
-	});
-});
-
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
@@ -21,6 +13,17 @@ app.use((req, res, next) => {
 app.get('/', (req,res) => {
 	res.json({message: "Success"});
 })
+
+app.get('/api/polls', (req, res) => {
+	mongo.connect(mongoURI, (err, db) => {
+		if (err) throw err;
+		console.log('Connected to database');
+		db.collection('polls').find().toArray((err, docs) => {
+			console.log(docs);
+			res.json(docs);
+		});
+	});
+});
 
 app.listen(3001, () => {
 	console.log('Listening on port 3001');
