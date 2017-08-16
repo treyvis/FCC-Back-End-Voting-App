48a513d2d1b3d5fd5ab77d0ff7a0f7781ea24e1a
Trey <treyvis@gmail.com>
2017-08-16 16:58:47 -0600

    Connecting to mongodb database

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 
 const mongo = require('mongodb').MongoClient;
+const config = require('./config.json');
+const mongoURI = 'mongodb://' + config.user + ':' + config.password + '@ds123351.mlab.com:23351/fcc-voting-app';
+
+mongo.connect(mongoURI, (err, db) => {
+	if (err) throw err;
+	console.log('Connected to database');
+	db.collection('polls').find().toArray((err, docs) => {
+		console.log(docs);
+	});
+});
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
