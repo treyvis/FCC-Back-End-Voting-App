ff9dd81e4197056f9d5fd264afdbe840b7d6c858
Trey <treyvis@gmail.com>
2017-08-16 17:16:15 -0600

    Pulling data dynamically from mongodb

--- src/pages/Polls.js
+++ src/pages/Polls.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { Segment, List } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import polls from '../database/polls.json'
+import polls from '../database/polls.json';
+import Axios from 'axios';
 
 class Polls extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			polls: []
+		};
+	}
+
+	componentWillMount() {
+		Axios.get('http://localhost:3001/api/polls').then((res) => {
+			console.log(res.data);
+			this.setState({polls: res.data});
+		}).catch((err) => {
+			console.log(err);
+		})
+	}
 
 	render() {
-		const pollsList = polls.data.map((poll, index) => {
+		const pollsList = this.state.polls.map((poll, index) => {
 			return (
 				<List.Item  key={index}>
 					<Link to={this.props.match.url + '/' + index} >
