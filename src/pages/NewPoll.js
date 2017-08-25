0b19c8aa1a4c8319aca8112a3084e71e60de9600
Trey <treyvis@gmail.com>
2017-08-24 23:13:28 -0600

    completed pull logic for creating removeing and updating choices

--- src/pages/NewPoll.js
+++ src/pages/NewPoll.js
@@ -10,25 +10,64 @@ class NewPoll extends Component {
 			choices: [
 				{name: ''},
 				{name: ''},
+				{name: ''},
+				{name: ''},
 				{name: ''}
 			]
 		};
 
 		this.nameUpdate = this.nameUpdate.bind(this);
+		this.descriptionUpdate = this.descriptionUpdate.bind(this);
+		this.choiceUpdate = this.choiceUpdate.bind(this);
+		this.addChoice = this.addChoice.bind(this);
+		this.removeChoice = this.removeChoice.bind(this);
 	}
 
-	nameUpdate(e){
-		this.setState({name: e.target.value});
+	nameUpdate(event){
+		this.setState({name: event.target.value});
 	}
+
+	descriptionUpdate(event){
+		this.setState({description: event.target.value});
+	}
+
+	choiceUpdate(event, index){
+		let choices = this.state.choices;
+		choices[index].name = event.target.value;
+		this.setState({choices});
+	}
+
+	addChoice(){
+		let choices = this.state.choices;
+		choices.push({name: ''});
+		this.setState(choices);
+	}
+
+	removeChoice(index){
+		let choices = this.state.choices;
+		choices.splice(index, 1);
+		this.setState(choices);
+	}
+
 	render(){
+
+		let additionalChoices = [];
+		for (let i = 2; i < this.state.choices.length; i++) {
+			additionalChoices.push(
+				<Input label={'Choice ' + (i + 1)} key={i}
+					value={this.state.choices[i].name} onChange={(event) => {this.choiceUpdate(event, i)}}
+					fluid icon={{name: 'remove circle', link: true, onClick: () => {this.removeChoice(i)}}}/>
+			);
+		}
+
 		return(
 			<Card>
 				<Input label='Name' value={this.state.name} onChange={this.nameUpdate} fluid />
-				<Input label='Description' fluid />
-				<Input label='Choice 1' fluid />
-				<Input label='Choice 2' fluid />
-				<Input label='Choice 3' fluid icon={{name: 'remove circle', link: true}}/>
-				<Button icon='add circle' />
+				<Input label='Description' value={this.state.description} onChange={this.descriptionUpdate} fluid />
+				<Input label='Choice 1' value={this.state.choices[0].name} onChange={(event) => {this.choiceUpdate(event,0)}} fluid />
+				<Input label='Choice 2' value={this.state.choices[1].name} onChange={(event) => {this.choiceUpdate(event,1)}}fluid />
+				{additionalChoices}
+				<Button icon='add circle' onClick={this.addChoice} />
 			</Card>
 		);
 	}
