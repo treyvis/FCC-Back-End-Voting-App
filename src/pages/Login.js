c7235be16370e9c45d472bcbb629fdb20c9b0d05
Trey <treyvis@gmail.com>
2017-09-19 20:46:12 -0600

    state changes for login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import { Card, Input, Button } from 'semantic-ui-react';
 
 class Login extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: ''
+		};
+
+		this.updateInput = this.updateInput.bind(this);
+		this.login = this.login.bind(this);
+	}
+
+	updateInput(event, input){
+		let state = this.state;
+		state[input] = event.target.value;
+		this.setState(state);
+	}
+
+	login() {
+
+	}
+
 	render(){
 		return(
 			<Card>
@@ -9,8 +31,12 @@ class Login extends Component {
 	            <Card.Header>
 	              Login!
 	            </Card.Header>
-	            <Input label='Email' fluid />
-	            <Input label='Password' fluid />
+	            <Input label='Email' fluid
+		            value={this.state.email} 
+		            onChange={(event) => {this.updateInput(event, 'email')}}/>
+	            <Input label='Password' fluid
+		            value={this.state.password} 
+		            onChange={(event) => {this.updateInput(event, 'password')}}/>
 	            <Button content='Submit' primary fluid/>
 	          </Card.Content>
 	        </Card>
